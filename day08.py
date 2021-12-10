351b250904272ad4d1945f139b5586f869af587f
Prashanth Patali <patali@gmail.com>
2021-12-11 00:43:21 +0530

    Phew! Day 8 finally completed

--- day08.py
+++ day08.py
@@ -1,38 +1,109 @@
 # https://adventofcode.com/2021/day/8
 
-from typing import List, Tuple
-
-MAP_DIGIT_SEGMENT_LENGTH = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
+from typing import List, Tuple, Dict, Set
 
 
-def ReadInputLines(inputLines: List[str]) -> List[Tuple[List[str], List[str]]]:
+def ReadInputLinesAsSets(
+    inputLines: List[str],
+) -> List[Tuple[List[Set[str]], List[Set[str]]]]:
     lines = []
     for inputLine in inputLines:
         signals, outputs = tuple(inputLine.strip().split(" | "))
-        lines.append((signals.split(), outputs.split()))
+        lines.append(
+            ([set(s) for s in signals.split()], [set(o) for o in outputs.split()])
+        )
     return lines
 
 
-def CountDigitsInOutput(
-    lines: List[Tuple[List[str], List[str]]], digitsToFind: List[int]
+def CountDigitsWithGivenSegmentLengthInOutput(
+    lines: List[Tuple[List[Set[str]], List[Set[str]]]],
+    digitsWithSegmentLengthToFind: List[int],
 ) -> int:
     count = 0
     for _, outputs in lines:
-        for output in outputs:
-            for digit in digitsToFind:
-                if len(output) == MAP_DIGIT_SEGMENT_LENGTH[digit]:
-                    count += 1
+        count += len([o for o in outputs if len(o) in digitsWithSegmentLengthToFind])
     return count
+
+
+def DecodeSignals(signals: List[Set[str]]) -> List[Set[str]]:
+    decodedSignals = [set() for _ in range(10)]
+
+    # Find those number whose number of segments is unique - 1,7,4,8
+    decodedSignals[1] = next(s for s in signals if len(s) == 2)  # 1
+    decodedSignals[7] = next(s for s in signals if len(s) == 3)  # 7
+    decodedSignals[4] = next(s for s in signals if len(s) == 4)  # 4
+    decodedSignals[8] = next(s for s in signals if len(s) == 7)  # 8
+
+    # Find those whose number of segments are of length 5 - 2,3,5
+
+    # Merge/Union of segments of 1 and 3 results in 3
+    decodedSignals[3] = next(
+        s for s in signals if len(s) == 5 and decodedSignals[1] | s == s
+    )  # 3
+
+    # Merge/Union of segments of 4 and 2 results in 8
+    decodedSignals[2] = next(
+        s
+        for s in signals
+        if len(s) == 5
+        and decodedSignals[4] | s == decodedSignals[8]
+        and s != decodedSignals[3]
+    )  # 2
+
+    # Remaining has to be 5
+    decodedSignals[5] = next(
+        s
+        for s in signals
+        if len(s) == 5 and s not in [decodedSignals[2], decodedSignals[3]]
+    )  # 5
+
+    # Find those whose number of segments are of length 6 - 0,6,9
+
+    # Merge/Union of segments of 1 and 3 results in 3
+    decodedSignals[6] = next(
+        s for s in signals if len(s) == 6 and decodedSignals[1] | s == decodedSignals[8]
+    )  # 6
+
+    # Merge/Union of segments of 4 and 2 results in 8
+    decodedSignals[0] = next(
+        s
+        for s in signals
+        if len(s) == 6
+        and decodedSignals[4] | s == decodedSignals[8]
+        and s != decodedSignals[6]
+    )  # 0
+
+    # Remaining has to be 9
+    decodedSignals[9] = next(
+        s
+        for s in signals
+        if len(s) == 6 and s not in [decodedSignals[0], decodedSignals[6]]
+    )  # 9
+
+    return decodedSignals
+
+
+def DecodeOutputs(outputs: List[Set[str]], decodeSignals: List[Set[str]]) -> int:
+    return int("".join([str(decodeSignals.index(o)) for o in outputs]))
+
+
+def Decode(lines: List[Tuple[List[Set[str]], List[Set[str]]]]) -> List[int]:
+    decodedOutputs = []
+    for signals, outputs in lines:
+        decodedSignals = DecodeSignals(signals)
+        decodedOutputs.append(DecodeOutputs(outputs, decodedSignals))
+    return decodedOutputs
 
 
 def main():
     with open("day08.input.txt", "rt") as inputFile:
         readlines = inputFile.readlines()
-        lines = ReadInputLines(readlines)
-        digitsToFind = [1, 4, 7, 8]
+        lines = ReadInputLinesAsSets(readlines)
         print(
-            f"Number of occurances of {digitsToFind} = {CountDigitsInOutput(lines, digitsToFind)}"
+            f"Number of occurances of 1, 4, 7, 8 = {CountDigitsWithGivenSegmentLengthInOutput(lines, [2, 3, 4, 7])}"
         )
+
+        print(f"Sum of all output values = {sum(Decode(lines))}")
 
 
 if __name__ == "__main__":
--- test_day08.py
+++ test_day08.py
@@ -15,13 +15,89 @@ def test_CountDigitsInOutput():
         "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
     ]
 
-    lines = ReadInputLines(input)
-    assert 26 == CountDigitsInOutput(lines, [1, 4, 7, 8])
+    lines = ReadInputLinesAsSets(input)
+    assert 26 == CountDigitsWithGivenSegmentLengthInOutput(lines, [2, 3, 4, 7])
 
 
 def test_Part1Result():
     with open("day08.input.txt", "rt") as inputFile:
         readlines = inputFile.readlines()
-        lines = ReadInputLines(readlines)
-        digitsToFind = [1, 4, 7, 8]
-        assert 512 == CountDigitsInOutput(lines, digitsToFind)
+        lines = ReadInputLinesAsSets(readlines)
+        digitsWithSegmentLengthToFind = [2, 3, 4, 7]
+        assert 512 == CountDigitsWithGivenSegmentLengthInOutput(
+            lines, digitsWithSegmentLengthToFind
+        )
+
+
+def test_ReadInputLinesAsSets():
+    input = [
+        "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    ]
+
+    lines = ReadInputLinesAsSets(input)
+    signals, outputs = lines[0]
+    assert signals == [
+        {"a", "c", "e", "d", "g", "f", "b"},
+        {"c", "d", "f", "b", "e"},
+        {"g", "c", "d", "f", "a"},
+        {"f", "b", "c", "a", "d"},
+        {"d", "a", "b"},
+        {"c", "e", "f", "a", "b", "d"},
+        {"c", "d", "f", "g", "e", "b"},
+        {"e", "a", "f", "b"},
+        {"c", "a", "g", "e", "d", "b"},
+        {"a", "b"},
+    ]
+    assert outputs == [
+        {"c", "d", "f", "e", "b"},
+        {"f", "c", "a", "d", "b"},
+        {"c", "d", "f", "e", "b"},
+        {"c", "d", "b", "a", "f"},
+    ]
+
+
+def test_DecodeSample1():
+    input = [
+        "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf"
+    ]
+
+    lines = ReadInputLinesAsSets(input)
+    assert [5353] == Decode(lines)
+
+
+def test_DecodeSample2():
+    input = [
+        "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+        "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+        "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+        "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+        "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+        "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+        "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+        "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+        "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+        "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce",
+    ]
+
+    lines = ReadInputLinesAsSets(input)
+    decodedOutputs = Decode(lines)
+    assert [
+        8394,
+        9781,
+        1197,
+        9361,
+        4873,
+        8418,
+        4548,
+        1625,
+        8717,
+        4315,
+    ] == decodedOutputs
+    assert 61229 == sum(decodedOutputs)
+
+
+def test_Part2Result():
+    with open("day08.input.txt", "rt") as inputFile:
+        readlines = inputFile.readlines()
+        lines = ReadInputLinesAsSets(readlines)
+        assert 1091165 == sum(Decode(lines))
