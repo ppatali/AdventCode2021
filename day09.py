b31cfd6b0b52286c6aa43df967169b9bdcaeb79a
Prashanth Patali <patali@gmail.com>
2021-12-11 14:00:16 +0530

    Refactoring

--- day09.py
+++ day09.py
@@ -3,33 +3,18 @@
 from typing import List, Tuple, Dict, Set
 
 
-IMPOSSIBLE_LOWPOINT_HEIGHT = 10
-INVALID_BASIN_HEIGHT = 9
-
-
 def FindLowPointCoordinates(heatmap: List[str]) -> List[Tuple[int, int]]:
     lowPointCoordinates = []
     for y in range(len(heatmap)):
         for x in range(len(heatmap[y])):
+            # Find if this is the local minimum
             if int(heatmap[y][x]) < min(
                 [
                     # check adjacent points
-                    int(
-                        heatmap[y][x - 1] if x - 1 >= 0 else IMPOSSIBLE_LOWPOINT_HEIGHT
-                    ),  # left
-                    int(
-                        heatmap[y][x + 1]
-                        if x + 1 < len(heatmap[y])
-                        else IMPOSSIBLE_LOWPOINT_HEIGHT
-                    ),  # right
-                    int(
-                        heatmap[y - 1][x] if y - 1 >= 0 else IMPOSSIBLE_LOWPOINT_HEIGHT
-                    ),  # top
-                    int(
-                        heatmap[y + 1][x]
-                        if y + 1 < len(heatmap)
-                        else IMPOSSIBLE_LOWPOINT_HEIGHT
-                    ),  # bottom
+                    int(heatmap[y][x - 1] if x - 1 >= 0 else 10),  # left
+                    int(heatmap[y][x + 1] if x + 1 < len(heatmap[y]) else 10),  # right
+                    int(heatmap[y - 1][x] if y - 1 >= 0 else 10),  # top
+                    int(heatmap[y + 1][x] if y + 1 < len(heatmap) else 10),  # bottom
                 ]
             ):
                 lowPointCoordinates.append((y, x))
@@ -52,46 +37,30 @@ def FindBasins(
     basins: List[List[int]] = [[] for _ in range(len(lowPointCoorinates))]
 
     for i in range(len(lowPointCoorinates)):
-        tobeProcessed: List[Tuple[int, int]] = [lowPointCoorinates[i]]
+        queue: List[Tuple[int, int]] = [lowPointCoorinates[i]]
 
         j = 0
-        while j < len(tobeProcessed):
-            y, x = tobeProcessed[j]
+        while j < len(queue):
+            y, x = queue[j]
 
             basins[i].append(int(heatmap[y][x]))  # add to basin
 
             # check adjacent points, if valid add to back of the queue
             # left
-            if (
-                x - 1 >= 0
-                and int(heatmap[y][x - 1]) != INVALID_BASIN_HEIGHT
-                and (y, x - 1) not in tobeProcessed
-            ):
-                tobeProcessed.append((y, x - 1))
+            if x - 1 >= 0 and int(heatmap[y][x - 1]) != 9 and (y, x - 1) not in queue:
+                queue.append((y, x - 1))
 
             # right
-            if (
-                x + 1 < len(heatmap[y])
-                and int(heatmap[y][x + 1]) != INVALID_BASIN_HEIGHT
-                and (y, x + 1) not in tobeProcessed
-            ):
-                tobeProcessed.append((y, x + 1))
+            if x + 1 < len(heatmap[y]) and int(heatmap[y][x + 1]) != 9 and (y, x + 1) not in queue:
+                queue.append((y, x + 1))
 
             # top
-            if (
-                y - 1 >= 0
-                and int(heatmap[y - 1][x]) != INVALID_BASIN_HEIGHT
-                and (y - 1, x) not in tobeProcessed
-            ):
-                tobeProcessed.append((y - 1, x))
+            if y - 1 >= 0 and int(heatmap[y - 1][x]) != 9 and (y - 1, x) not in queue:
+                queue.append((y - 1, x))
 
             # bottom
-            if (
-                y + 1 < len(heatmap)
-                and int(heatmap[y + 1][x]) != INVALID_BASIN_HEIGHT
-                and (y + 1, x) not in tobeProcessed
-            ):
-                tobeProcessed.append((y + 1, x))
+            if y + 1 < len(heatmap) and int(heatmap[y + 1][x]) != 9 and (y + 1, x) not in queue:
+                queue.append((y + 1, x))
 
             j += 1
 
@@ -99,9 +68,9 @@ def FindBasins(
 
 
 def Compute3LargestBasinsWeight(basins: List[List[int]]) -> int:
-    basinsSizes = [len(basin) for basin in basins]
-    basinsSizes.sort(reverse=True)
-    return basinsSizes[0] * basinsSizes[1] * basinsSizes[2]
+    basinSizes = [len(basin) for basin in basins]
+    basinSizes.sort(reverse=True)
+    return basinSizes[0] * basinSizes[1] * basinSizes[2]
 
 
 def main():
