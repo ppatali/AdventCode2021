ea83faf535dae52d173158e8830a802e1c1d0799
Prashanth Patali <patali@gmail.com>
2021-12-13 22:43:03 +0530

    Day 13 with an efficient method

--- src/day13.py
+++ src/day13.py
@@ -3,10 +3,10 @@
 from typing import List, Tuple, Dict, Set
 
 
-def ReadInput(lines: List[str]) -> Tuple[List[str], List[Tuple[str, int]]]:
-    xPoints: List[int] = []
-    yPoints: List[int] = []
+def ReadInput(lines: List[str]) -> Tuple[List[str], List[Tuple[int, int]], List[Tuple[str, int]]]:
     folds: List[Tuple[str, int]] = []
+    points: List[Tuple[int, int]] = []
+    maxX, maxY = -1, -1
 
     for line in lines:
         if line is None or len(line.strip()) == 0:
@@ -16,18 +16,18 @@ def ReadInput(lines: List[str]) -> Tuple[List[str], List[Tuple[str, int]]]:
             folds.append((foldAlong, int(foldAt)))
         else:
             x, y = line.strip().split(",")
-            xPoints.append(int(x))
-            yPoints.append(int(y))
+            maxX, maxY = max(maxX, int(x)), max(maxY, int(y))
+            points.append((int(x),int(y)))
 
-    matrix = [[0 for _ in range(max(xPoints) + 1)] for _ in range(max(yPoints) + 1)]
+    matrix = [[0 for _ in range(maxX + 1)] for _ in range(maxY + 1)]
 
-    for i in range(len(xPoints)):
-        matrix[yPoints[i]][xPoints[i]] = 1
+    for (x, y) in points:
+        matrix[y][x] = 1
 
-    return matrix, folds
+    return matrix, points, folds
 
 
-def FoldOnce(matrix: List[List[int]], foldAlong: str, foldAt: int) -> List[str]:
+def Fold(matrix: List[List[int]], foldAlong: str, foldAt: int) -> List[List[int]]:
     maxX, maxY = len(matrix[0]), len(matrix)
     # Fold vertically?
     if foldAlong == "x":
@@ -53,21 +53,64 @@ def FoldOnce(matrix: List[List[int]], foldAlong: str, foldAt: int) -> List[str]:
 def FoldAll(matrix: List[List[int]], folds: List[Tuple[str, int]]) -> List[List[int]]:
     output = matrix
     for foldAlong, foldAt in folds:
-        output = FoldOnce(output, foldAlong, foldAt)
+        output = Fold(output, foldAlong, foldAt)
+    return output
+
+# In this methods, the points are not plotted on a matrix
+# the logic instead uses only the list of points
+# Hence it is both space and time efficient as compared to using matrix
+# The logic is to check the point with respect to axis of folding
+#   if right side or bottom of the axis, then add the reflection of that point to list
+def FoldUsingPoints(points: List[Tuple[int, int]], foldAlong: str, foldAt: int) -> List[Tuple[int, int]]:
+    output = []
+    LARGE_NUM = 1000000000
+    foldAxisX = foldAt if foldAlong == "x" else LARGE_NUM
+    foldAxisY = foldAt if foldAlong == "y" else LARGE_NUM
+    for x, y in points:
+        # If right of axis, compute the reflection
+        x1 = x if x < foldAxisX else foldAxisX - (x - foldAxisX)
+        # If bottom of axis, compute the reflection
+        y1 = y if y < foldAxisY else foldAxisY - (y - foldAxisY)
+        if (x1, y1) not in output:
+            output.append((x1, y1))
+    return output
+
+
+def FoldAllUsingPoints(points: List[Tuple[int, int]], folds: List[Tuple[str, int]]) -> List[List[int]]:
+    output = points
+    for foldAlong, foldAt in folds:
+        output = FoldUsingPoints(output, foldAlong, foldAt)
     return output
 
 
 def main():
     with open("day13.input.txt", "rt") as inputFile:
         readlines = [line.strip() for line in inputFile.readlines()]
-        matrix, folds = ReadInput(readlines)
-        folded = FoldOnce(matrix, folds[0][0], folds[0][1])
+        matrix, points, folds = ReadInput(readlines)
+        
+        folded = Fold(matrix, folds[0][0], folds[0][1])
         count = sum([sum(x) for x in folded])
-        print(f"Part 1 - Number of dots visible after first fold instruction = {count}")
+        print(f"Part 1 - Method A - Number of dots visible after first fold instruction = {count}")
+        
+        folded = FoldUsingPoints(points, folds[0][0], folds[0][1])
+        print(f"Part 1 - Method B - Number of dots visible after first fold instruction = {len(folded)}")
 
+        print(f"Part 2 - Method A")
         folded = FoldAll(matrix, folds)
         for y in range(len(folded)):
             print("".join(["#" if x == 1 else " " for x in folded[y]]))
+
+        print(f"Part 2 - Method B")
+        folded = FoldAllUsingPoints(points, folds)
+        
+        # make matrix of points and print
+        maxX = max([x for x, _ in folded])
+        maxY = max([y for _, y in folded])
+        for y in range(maxY + 1):
+            line = ""
+            for x in range(maxX + 1):
+                line += "#" if (x, y) in folded else " "
+            print(line)
 
         # Expected output when printed - UEFZCUCJ
         #  # #### #### ####  ##  #  #  ##    ##
--- src/test_day13.py
+++ src/test_day13.py
@@ -42,12 +42,12 @@ def test_ReadInput():
         [1,0,0,0,0,0,0,0,0,0,0],
         [1,0,1,0,0,0,0,0,0,0,0],
     ]
-    matrix, folds = ReadInput(input)
+    matrix, _, folds = ReadInput(input)
 
     assert expectedOutput == matrix
     assert [('y', 7), ('x', 5)] == folds
 
-def test_FoldOnce():
+def test_Fold():
     input = [
         "6,10",
         "0,14",
@@ -91,21 +91,40 @@ def test_FoldOnce():
         [0,0,0,0,0],
         [0,0,0,0,0],
     ]
-    matrix, folds = ReadInput(input)
+
+    matrix, points, folds = ReadInput(input)
     
-    folded1 = FoldOnce(matrix, folds[0][0], folds[0][1])
+    folded1 = Fold(matrix, folds[0][0], folds[0][1])
     assert expectedOutput1 == folded1
 
-    folded2 = FoldOnce(folded1, folds[1][0], folds[1][1])
+    folded2 = Fold(folded1, folds[1][0], folds[1][1])
+    assert expectedOutput2 == folded2
+
+    expectedOutput1 = [
+        (6, 4),(0, 0),(9, 4),(0, 3),(10, 4),(4, 3),(6, 0),(6, 2),(4, 1),
+        (0, 1),(10, 2),(3, 4),(3, 0),(8, 4),(1, 4),(2, 0),(9, 0)
+    ]
+
+    folded1 = FoldUsingPoints(points, folds[0][0], folds[0][1])
+    assert expectedOutput1 == folded1
+
+    expectedOutput2 = [
+        (4, 4), (0, 0), (1, 4), (0, 3), (0, 4), (4, 3), (4, 0), (4, 2), 
+        (4, 1), (0, 1), (0, 2), (3, 4), (3, 0), (2, 4), (2, 0), (1, 0)
+    ]
+    folded2 = FoldUsingPoints(folded1, folds[1][0], folds[1][1])
     assert expectedOutput2 == folded2
 
 def test_Part1Result():
     with open("src/day13.input.txt", "rt") as inputFile:
         readlines = [line.strip() for line in inputFile.readlines()]
-        matrix, folds = ReadInput(readlines)
-        folded = FoldOnce(matrix, folds[0][0], folds[0][1])
+        matrix, points, folds = ReadInput(readlines)
+        folded = Fold(matrix, folds[0][0], folds[0][1])
         count = sum([sum(x) for x in folded])
         assert 669 == count
+
+        folded = FoldUsingPoints(points, folds[0][0], folds[0][1])
+        assert 669 == len(folded)
 
 def test_Part2Result():
     expectedOutput = [
@@ -117,6 +136,18 @@ def test_Part2Result():
         [0,1,1,0,0,1,1,1,1,0,1,0,0,0,0,1,1,1,1,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0],
     ]
     
+    expectedPoints = [
+        (17, 0),(32, 5),(12, 2),(10, 2),(7, 0),(26, 5),(17, 2),(28, 0),(32, 0),(13, 0),
+        (38, 0),(7, 2),(0, 0),(20, 4),(16, 3),(23, 4),(27, 5),(16, 0),(37, 5),(5, 2),
+        (3, 4),(25, 4),(31, 0),(17, 5),(15, 0),(5, 4),(8, 5),(37, 0),(15, 5),(7, 5),
+        (30, 3),(38, 1),(18, 5),(36, 5),(33, 1),(38, 4),(33, 4),(6, 2),(28, 1),(18, 0),
+        (22, 0),(25, 1),(5, 1),(20, 3),(0, 1),(5, 3),(28, 2),(8, 0),(3, 1),(25, 3),(1, 5),
+        (30, 1),(20, 1),(25, 0),(3, 2),(10, 5),(2, 5),(35, 4),(6, 5),(6, 0),(38, 3),(0, 2),
+        (28, 3),(3, 3),(11, 2),(28, 4),(3, 0),(12, 0),(5, 5),(16, 5),(15, 4),(22, 5),(10, 1),
+        (31, 5),(10, 3),(5, 0),(10, 0),(0, 4),(30, 4),(25, 2),(0, 3),(20, 2),(23, 1),(10, 4),
+        (21, 5),(18, 1),(21, 0),(38, 2),(30, 2),(11, 0)
+    ]
+
     #Expected output when printed - UEFZCUCJ
     #  # #### #### ####  ##  #  #  ##    ## 
     #  # #    #       # #  # #  # #  #    # 
@@ -126,6 +157,9 @@ def test_Part2Result():
      ##  #### #    ####  ##   ##   ##   ##
     with open("src/day13.input.txt", "rt") as inputFile:
         readlines = [line.strip() for line in inputFile.readlines()]
-        matrix, folds = ReadInput(readlines)
+        matrix, points, folds = ReadInput(readlines)
         folded = FoldAll(matrix, folds)
-        assert expectedOutput == folded #
+        assert expectedOutput == folded
+
+        folded = FoldAllUsingPoints(points, folds)
+        assert folded == expectedPoints
