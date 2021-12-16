76d88f7d6e9f5581ea318fbe55ab803e7132e508
Prashanth Patali <patali@gmail.com>
2021-12-16 12:58:26 +0530

    Used Counter to simplify code

--- src/day14.py
+++ src/day14.py
@@ -1,26 +1,18 @@
 # https://adventofcode.com/2021/day/14
 
+from collections import Counter
 from typing import List, Tuple, Dict, Set
 
 
 def ReadInput(
     lines: List[str],
 ) -> Tuple[Dict[Tuple[str, str], int], Dict[Tuple[str, str], str]]:
-    # First line is the template
-    template = {}
-    templateline = lines[0]
-    for i in range(len(templateline) - 1):
-        elem1, elem2 = templateline[i], templateline[i + 1]
-        template[(elem1, elem2)] = (
-            1 if (elem1, elem2) not in template else template[(elem1, elem2)] + 1
-        )
-
+    # Parse the first line as template
+    template = Counter([tuple(lines[0][i : i + 2]) for i in range(len(lines[0]) - 1)])
+    
     # Now read all the pair insertion, skipping any lines that does not include "->"
-    pairInsertions = {}
-    for line in lines:
-        if line.find("->") != -1:
-            pairchar, insertchar = line.strip().split(" -> ")
-            pairInsertions[(pairchar[0], pairchar[1])] = insertchar
+    pairInsertions = dict([line.strip().split(" -> ") for line in lines if line.find("->") != -1])
+    pairInsertions = {(key[0], key[1]) : value for key, value in pairInsertions.items()}
 
     return template, pairInsertions
 
@@ -28,30 +20,18 @@ def ReadInput(
 def StepThrough(
     chain: Dict[Tuple[str, str], int], rules: Dict[Tuple[str, str], str]
 ) -> Dict[Tuple[str, str], int]:
-    newChain: Dict[Tuple[str, str], int] = {}
+    newChain: Dict[Tuple[str, str], int] = Counter()
     for (elem1, elem2), count in chain.items():
         elem3 = rules[(elem1, elem2)]
-        
-        newChain[(elem1, elem3)] = (
-            count
-            if (elem1, elem3) not in newChain
-            else newChain[(elem1, elem3)] + count
-        )
-        
-        newChain[(elem3, elem2)] = (
-            count
-            if (elem3, elem2) not in newChain
-            else newChain[(elem3, elem2)] + count
-        )
+        newChain[(elem1, elem3)] += count        
+        newChain[(elem3, elem2)] += count
     return newChain
 
 
 def CountElements(chain, lastTemplateElem: str) -> Dict[str, int]:
-    elemCounts = {}
+    elemCounts = Counter()
     for (elem1, elem2), count in chain.items():
-        elemCounts[elem1] = (
-            count if elem1 not in elemCounts else elemCounts[elem1] + count
-        )
+        elemCounts[elem1] += count
     # for the last element in the template, add 1
     elemCounts[lastTemplateElem] += 1
     return elemCounts
