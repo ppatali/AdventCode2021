e15053cc23042f7ac95f10e61d9ceda26da5c4ee
Prashanth Patali <patali@gmail.com>
2021-12-22 16:56:37 +0530

    Day 21 Part 1 completed using brute force approach

---
+++ src/test_day21.py
@@ -0,0 +1,9 @@
+from day21 import *
+
+def test_Part1_Sample():
+    assert 739785 == play(4, 8, dice_deterministic) 
+
+
+def test_Part1Result():
+    assert 802452 == play(7, 10, dice_deterministic)
+
